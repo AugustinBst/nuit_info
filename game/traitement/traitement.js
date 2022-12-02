0961194835f12843511e5ac6968c5d339e7de231
Sebastien <sebastien.bertil-souchet@epitech.eu>
2022-12-02 07:23:22 +0100

    CSS jeux

--- game/traitement/traitement.js
+++ game/traitement/traitement.js
@@ -1,5 +1,5 @@
 var lock = 0;
-var text_win = "GG et oui c'est comme sa le VIH"
+var text_win = "GG et oui c'est comme sa le traitement"
 var text_lose = "Passer sur le site de aides : https://www.aides.org/"
 
 function first(element1, element2, element3){
@@ -7,7 +7,7 @@ function first(element1, element2, element3){
         element1.style.backgroundColor = "lightgreen";
         element2.style.backgroundColor = "red";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_win;
+        document.getElementById("traitement_an").innerHTML = text_win;
         lock = 1;
     }
 }
@@ -17,7 +17,7 @@ function second(element1, element2, element3){
         element1.style.backgroundColor = "lightgreen";
         element2.style.backgroundColor = "red";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
@@ -27,7 +27,7 @@ function third(element1, element2, element3){
         element1.style.backgroundColor = "lightgreen";
         element2.style.backgroundColor = "red";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
@@ -37,7 +37,7 @@ function first_2(element1, element2, element3){
         element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "lightgreen";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
@@ -47,7 +47,7 @@ function second_2(element1, element2, element3){
         element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "lightgreen";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_win;
+        document.getElementById("traitement_an").innerHTML = text_win;
         lock = 1;
     }
 }
@@ -57,37 +57,38 @@ function third_2(element1, element2, element3){
         element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "lightgreen";
         element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
 
+
 function first_3(element1, element2, element3){
     if (lock === 0) {
-        element1.style.backgroundColor = "lightgreen";
+        element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "red";
-        element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_win;
+        element3.style.backgroundColor = "lightgreen";
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
 
 function second_3(element1, element2, element3){
     if (lock === 0) {
-        element1.style.backgroundColor = "lightreen";
+        element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "red";
-        element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        element3.style.backgroundColor = "lightgreen";
+        document.getElementById("traitement_an").innerHTML = text_lose;
         lock = 1;
     }
 }
 
 function third_3(element1, element2, element3){
     if (lock === 0) {
-        element1.style.backgroundColor = "lightgreen";
+        element1.style.backgroundColor = "red";
         element2.style.backgroundColor = "red";
-        element3.style.backgroundColor = "red";
-        document.getElementById("answer_vih").innerHTML = text_lose;
+        element3.style.backgroundColor = "lightgreen";
+        document.getElementById("traitement_an").innerHTML = text_win;
         lock = 1;
     }
 }
