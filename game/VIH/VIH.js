23b76b036d075b8d2c283a1d4af6374702309688
Sebastien <sebastien.bertil-souchet@epitech.eu>
2022-12-02 06:46:58 +0100

    VIH

---
+++ game/VIH/VIH.js
@@ -0,0 +1,93 @@
+var lock = 0;
+var text_win = "GG et oui c'est comme sa le VIH"
+var text_lose = "Et non, ce n'est pas comme sa."
+
+function first(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightgreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_win;
+        lock = 1;
+    }
+}
+
+function second(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightgreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
+
+function third(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightgreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
+
+function first_2(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "red";
+        element2.style.backgroundColor = "lightgreen";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
+
+function second_2(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "red";
+        element2.style.backgroundColor = "lightgreen";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_win;
+        lock = 1;
+    }
+}
+
+function third_2(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "red";
+        element2.style.backgroundColor = "lightgreen";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
+
+function first_3(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightgreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_win;
+        lock = 1;
+    }
+}
+
+function second_3(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
+
+function third_3(element1, element2, element3){
+    if (lock === 0) {
+        element1.style.backgroundColor = "lightgreen";
+        element2.style.backgroundColor = "red";
+        element3.style.backgroundColor = "red";
+        document.getElementById("answer_vih").innerHTML = text_lose;
+        lock = 1;
+    }
+}
